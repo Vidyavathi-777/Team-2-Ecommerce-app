4d6eb290c1db77def426207cf0108d47a4a5dcbc
Vidyavathi-777 <vidyavathidigadari_jtbb2@jtdfoundation.org>
2025-02-22 16:18:25 +0530

    addd port

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ app.use('/users', userRoutes);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
