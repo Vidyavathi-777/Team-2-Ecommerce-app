74dcb0bc0ce20fe30d869d1f59ccc72366ba5a67
Vidyavathi-777 <vidyavathidigadari_jtbb2@jtdfoundation.org>
2025-02-23 12:56:20 +0530

    fix missing dependencies

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use('/users', userRoutes);
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0",() => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
 
 module.exports = app;
